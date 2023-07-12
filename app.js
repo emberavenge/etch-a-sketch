e58fbea4edd691db731b88d8af6e3867b068a6ca
Emerson Castillo <emberavenge@protonmail.com>
2023-07-12 00:43:09 -0400

    Added JS, oops

--- app.js
+++ app.js
@@ -1,7 +1,48 @@
-const container = document.querySelectorByClassName(".container");
+const container = document.querySelector(".container");
+for (let i = 0; i < 16 * 16; i++) {
+  const colDiv = document.createElement("div");
+  colDiv.classList.add("highlight");
+  colDiv.style.backgroundColor = "white";
+  colDiv.style.border = "solid 1px black";
+  colDiv.style.flex = "1 calc(100%/16)";
+  colDiv.addEventListener(
+    "mouseenter",
+    function () {
+      colDiv.style.backgroundColor = "black";
+    },
+    true
+  );
 
-const div = document.createElement("div");
+  container.appendChild(colDiv);
+}
 
-div.body.style.backgroundColor = "'black";
 
-container.appendChild(div);
+
+function generateGrid(num) {
+  if (num < 100) {
+    for (let i = 0; i < num*num; i++) {
+      const colDiv = document.createElement("div");
+      colDiv.classList.add("highlight");
+      colDiv.style.backgroundColor = "white";
+      colDiv.style.border = "solid 1px black";
+      colDiv.style.flex = "1 calc(100%/16)";
+      colDiv.addEventListener(
+        "mouseenter",
+        function () {
+          colDiv.style.backgroundColor = "black";
+        },
+        true
+      );
+
+      container.appendChild(colDiv);
+    }
+  } else {
+    return 0;
+  }
+}
+
+
+// func userGrid () {
+//   let playGrid = prompt('Enter a number between 1-100');
+//   let num = parseInt(playGrid);
+// }
