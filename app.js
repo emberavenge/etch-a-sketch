069b2515f526f86e94656b16c20b91b94b661466
Emerson Castillo <61599413+emberavenge@users.noreply.github.com>
2023-07-14 17:46:16 -0400

    Update app.js and added a clear button function

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const container = document.querySelector(".container");
+let clicked = false;
+
 for (let i = 0; i < 16 * 16; i++) {
   const colDiv = document.createElement("div");
   colDiv.classList.add("highlight");
@@ -6,8 +8,9 @@ for (let i = 0; i < 16 * 16; i++) {
   colDiv.style.border = "solid 1px black";
   colDiv.style.flex = "1 calc(100%/16)";
   colDiv.addEventListener(
-    "mouseenter",
+    "mouseover",
     function () {
+      clicked = true;
       colDiv.style.backgroundColor = "black";
     },
     true
@@ -18,16 +21,27 @@ for (let i = 0; i < 16 * 16; i++) {
 
 
 
-function generateGrid(num) {
+  const removeChilds = (parent) => {
+    while (parent.lastChild) {
+        parent.removeChild(parent.lastChild);
+    }
+
+  }
+
+
+
+function generateGrid() {
+  let playGrid = prompt("Enter a number between 1-100");
+  let num = parseInt(playGrid);
+
   if (num < 100) {
-    for (let i = 0; i < num*num; i++) {
+    for (let i = 0; i < num * num; i++) {
       const colDiv = document.createElement("div");
-      colDiv.classList.add("highlight");
       colDiv.style.backgroundColor = "white";
       colDiv.style.border = "solid 1px black";
-      colDiv.style.flex = "1 calc(100%/16)";
+      colDiv.style.flex = `1 calc(100%/${num})`;
       colDiv.addEventListener(
-        "mouseenter",
+        "mouseover",
         function () {
           colDiv.style.backgroundColor = "black";
         },
@@ -41,8 +55,17 @@ function generateGrid(num) {
   }
 }
 
+const btnGen = document
+  .querySelector(".generate")
+  .addEventListener("click", function () {
+    clicked = true;
+    removeChilds(container);
+    generateGrid();
+  });
 
-// func userGrid () {
-//   let playGrid = prompt('Enter a number between 1-100');
-//   let num = parseInt(playGrid);
-// }
+document.querySelector(".clear").addEventListener("click", function () {
+  const squares = document.querySelectorAll(".container div");
+  squares.forEach((square) => {
+    square.style.backgroundColor = "white";
+  });
+});
